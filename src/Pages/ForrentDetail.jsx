d7a64744f9a24a8e76b58ec48fff8385cbb82a17
bobbb1999 <50527401+bobbb1999@users.noreply.github.com>
2023-09-19 16:53:46 +0700

    page detail

---
+++ src/Pages/ForrentDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Forrent_Detail from '../Components/Forrent_Detail'
+import Navbar_Login from '../Components/Navbar_Login'
+
+function ForrentDetail() {
+  return (
+    <>
+    <Navbar_Login />
+    <Forrent_Detail />
+    </>
+  )
+}
+
+export default ForrentDetail
---
+++ src/Pages/PhotographerDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Photographer_Detail from '../Components/Photographer_Detail'
+import Navbar_Login from '../Components/Navbar_Login'
+
+function PhotographerDetail() {
+  return (
+    <>
+    <Navbar_Login />
+    <Photographer_Detail />
+    </>
+  )
+}
+
+export default PhotographerDetail
