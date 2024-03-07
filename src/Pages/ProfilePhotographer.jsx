a50341b16965ddf4daca05606d6a118a42840c0c
bobbb1999 <basnarish1999@gmail.com>
2024-03-08 03:18:58 +0700

    show profile

---
+++ src/Pages/ProfilePhotographer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Profile_Photographer from '../Components/Profile_Photographer'
+import Navbar_Login from '../Components/Navbar_Login'
+
+function ProfilePhotographer() {
+  return (
+    <div>
+        <Navbar_Login />
+      <Profile_Photographer />
+    </div>
+  )
+}
+
+export default ProfilePhotographer
