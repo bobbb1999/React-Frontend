5e2ab2c0ae1268464dea2fd3bbbe8ebc870da4f0
bobbb1999 <basnarish1999@gmail.com>
2024-03-08 03:18:47 +0700

    edit profie

---
+++ src/Pages/ProfileEditPhotographer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Profile_Photo_EditForm from '../Components/Profile_Photo_EditForm'
+import Navbar_Login from '../Components/Navbar_Login'
+
+function ProfileEditPhotographer() {
+  return (
+    <div>
+        <Navbar_Login />
+      <Profile_Photo_EditForm />
+    </div>
+  )
+}
+
+export default ProfileEditPhotographer
